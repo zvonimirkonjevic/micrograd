43df63db1e129651119d03ef227e01ca40af5ebe
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-15 18:20:29 +0100

    implement check to see if other parameter in addition and multiplication are Value object and if they are not create new Value object

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -11,6 +11,7 @@ class Value:
         return f"Value(data={self.data})"
 
     def __add__(self, other):
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
         def _backward():
             self.grad += 1.0 * out.grad
@@ -19,6 +20,7 @@ class Value:
         return out
     
     def __mul__(self, other):
+        other = other if isinstance(other,Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
         def _backward():
             self.grad += other.grad * out.grad
