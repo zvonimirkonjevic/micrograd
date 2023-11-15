129d5dc4d033ca1934530c62d3012f2fc34e644d
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-15 17:59:08 +0100

    implement backward propagation

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -3,6 +3,7 @@ class Value:
     def __init__(self, data, _children=(), _op=''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
 
@@ -11,14 +12,41 @@ class Value:
 
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
         return out
     
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
+        def _backward():
+            self.grad += other.grad * out.grad
+            other.grad += self.grad * out.grad
+        out._backward = _backward
         return out
 
     def tanh(self):
         x = self.data
         t = (math.exp(2*x)-1)/(math.exp(2*x)+1)
         out = Value(t,(self,))
+        def _backward():
+            self.grad += (1-t**2) * out.grad
+        out._backward = _backward
         return out
+
+    def backward(self):
+
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1.0
+        for node in reversed(topo):
+            node._backward()
