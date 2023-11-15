4a0a892e463caa615af1ab18c47fec8e02beb6fe
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-15 17:44:49 +0100

    create variable grad that stores value of how much object has impact on the output

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -2,7 +2,9 @@ class Value:
 
     def __init__(self, data, _children=(), _op=''):
         self.data = data
+        self.grad = 0.0
         self._prev = set(_children)
+        self._op = _op
 
     def __repr__(self):
         return f"Value(data={self.data})"
