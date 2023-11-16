7ea5973903245233266fe7093ba8e69c70cef33b
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-16 18:40:35 +0100

    implement fetching of parameters w and b of neural network

--- micrograd/nnlib.py
+++ micrograd/nnlib.py
@@ -11,6 +11,9 @@ class Neuron:
         out = act.tanh()
         return out
 
+    def parameters(self):
+        return self.w + [self.b]
+
 class Layer:
     def __init__(self, input_size, layer_size):
         self.neurons = [Neuron(input_size) for _ in range(layer_size)]
@@ -18,6 +21,9 @@ class Layer:
     def __call__(self, x):
         outs = [n(x) for n in self.neurons]
         return outs[0] if len(outs)==1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
 
 class MLP:
     def __init__(self, input_size, layers_sizes):
@@ -28,3 +34,6 @@ class MLP:
         for layer in self.layers:
             x = Layer(x)
         return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
