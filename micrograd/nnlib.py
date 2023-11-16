459a41573ca879a7937a3639178ac9206d750a46
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-16 16:37:39 +0100

    implement neuron layer

--- micrograd/nnlib.py
+++ micrograd/nnlib.py
@@ -10,3 +10,13 @@ class Neuron:
         act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
         out = act.tanh()
         return out
+
+class Layer:
+    def __init__(self, input_size, layer_size):
+        self.neurons = [Neuron(input_size) for _ in range(layer_size)]
+        
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs)==1 else outs
+
+
