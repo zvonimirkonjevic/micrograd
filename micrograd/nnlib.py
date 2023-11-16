6e6319d03f67d9575f8c351d5d155a437b5ab904
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2023-11-16 16:36:39 +0100

    implement neuron

--- micrograd/nnlib.py
+++ micrograd/nnlib.py
@@ -1,4 +1,12 @@
 import random
 from micrograd.engine import Value
 
+class Neuron:
+    def __init__(self, input_size):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(input_size)]
+        self.b = Value(random.uniform(-1,1))
 
+    def __call__(self, x):
+        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
