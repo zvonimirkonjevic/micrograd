423ad51922a24699a2005745a50ee66992cd3ab5
zvonimirkonjevic <zvonimir.konjevic@gmail.com>
2026-08-27 09:49:26 +0200

    chore: adopt uv and move package to src layout
    Add pyproject.toml, .python-version and uv.lock, and move the package
    from micrograd/ to src/, updating nn.py's import path accordingly.
    
    Drops the RNN classes (RNNNeuron, RNNLayer, RNN) that were not carried
    over in the move.

---
+++ src/nn.py
@@ -0,0 +1,64 @@
+import random
+from src.engine import Value
+
+class Module:
+
+    def zero_grad(self):
+        for p in self.parameters():
+            p.grad = 0.0
+
+    def parameters(self):
+        return []
+
+
+# 
+#   Neurons 
+#
+
+class Neuron(Module):
+    def __init__(self, input_size):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(input_size)]
+        self.b = Value(random.uniform(-1,1))
+
+    def __call__(self, x):
+        act = sum((wi*xi for wi,xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+
+    def parameters(self):
+        return self.w + [self.b]
+
+
+# 
+#   Layers 
+#
+
+class Layer(Module):
+    def __init__(self, input_size, layer_size):
+        self.neurons = [Neuron(input_size) for _ in range(layer_size)]
+        
+    def __call__(self, x):
+        outs = [n(x) for n in self.neurons]
+        return outs[0] if len(outs)==1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+
+
+# 
+#   Architectures 
+#
+
+
+class MLP(Module):
+    def __init__(self, input_size, layers_sizes):
+        sz = [input_size] + layers_sizes
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(layers_sizes))]
+
+    def __call__(self, x):
+        for layer in self.layers:
+            x = Layer(x)
+        return x
+
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
